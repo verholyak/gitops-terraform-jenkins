e9dda2ca57154ad15c71907a451b3af2355186b1
verholyak <61271519+verholyak@users.noreply.github.com>
2020-03-24 23:47:26 +0200

    Update main.tf
    change instans

--- main.tf
+++ main.tf
@@ -13,13 +13,16 @@ provider "aws" {
 }
 
 # Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = var.ami
-  count                  = var.instance_count
-  key_name               = var.key_name
+esource "aws_instance" "my_webserver" {
+  ami           = "ami-0ba441bdd9e494102"
+  instance_type = "t2.micro"
+# resource "aws_instance" "default" {
+#  ami                    = var.ami
+#  count                  = var.instance_count
+#  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
-  source_dest_check      = false
-  instance_type          = var.instance_type
+#  source_dest_check      = false
+#  instance_type          = var.instance_type
 
   tags = {
     Name = "terraform-default"
